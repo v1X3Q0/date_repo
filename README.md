b3add7a28006bc5a804f58d136c00a28ffbb7a10
David Riusech <v1X3Q0@gmail.com>
2022-01-28 03:06:17 -0500

    working and output

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # date_repo
 repo and submodules aren't dated, allow request to pull a submodule at a date.
+
+## running
+This tool can be run from within an initialized repo, or to "init" a repo and pull its dependencies. Full support is not yet available for initializing a repo, but a sample command line for use from within a branch of the android common kernel:
+`python3 ~/repos/date_repo/date_repo.py --date 2021-07-06`
+
+## sample output:
+```shell
+mariomain@raichu-off:~/raichu-storage/repos/common-android11-5.4$ python3 ~/repos/date_repo/date_repo.py --date 2021-07-06
+/home/mariomain/repos/date_repo/date_repo.py:66: DeprecationWarning: This method will be removed in future versions.  Use 'list(elem)' or iteration over elem instead.
+  for i in default_xml.getchildren():
+repo build checking out commit 7d8f18edb79f3d3d85966226e5b23ae57669b6c0 dated 2021-07-01 10:06:57 +0000
+repo common checking out commit f8878f4c597b94c91e06592105f6e60febd4125c dated 2021-07-05 12:31:35 +0530
+repo common-modules/virtual-device checking out commit e40ae31320073a57c04c40b916f84801c291f06f dated 2021-06-18 22:06:14 +0200
+repo hikey-modules checking out commit 33d4b7f884500689a297df156b1c0a29e1c525da dated 2020-08-25 19:57:50 +0800
+WARNING: no commit for repo prebuilts/kernel-build-tools before 2021-07-06, earliest 2021-07-23 17:31:18 +0100
+WARNING: no commit for repo prebuilts/build-tools before 2021-07-06, earliest 2021-07-29 11:46:04 +0000
+WARNING: no commit for repo prebuilts-master/clang/host/linux-x86 before 2021-07-06, earliest 2021-07-26 11:46:32 +0100
+repo prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 checking out commit 98dce673ad97a9640c5d90bbb1c718e75c21e071 dated 2020-04-10 04:19:19 +0000
+repo prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 checking out commit 3e1d1fd459f5bb401479602c44448764f54ffe57 dated 2020-04-10 04:28:39 +0000
+WARNING: no commit for repo prebuilts/gcc/linux-x86/x86/x86_64-linux-android-4.9 before 2021-07-06, earliest 2021-07-26 11:56:07 +0100
+repo tools/mkbootimg checking out commit d0d261f3b0f57105f570a9878e748d817a3c5e60 dated 2021-05-05 15:10:50 +0800
+```
--- date_repo.py
+++ date_repo.py
@@ -2,67 +2,91 @@ import re
 import git
 import os
 import argparse
+from subprocess import PIPE
+from subprocess import Popen
 import xml.parsers.expat
 import xml.etree.ElementTree as ET
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument("--date", "-d", required=True,
     help="date in the unix format YYYY-MM-DD")
-parser.add_argument("--url", "-u", required=True,
+parser.add_argument("--url", "-u",
     help="url provided")
 parser.add_argument("--gitrepo", "-g",
     help="target git repo to date")
 parser.add_argument("--repman", "-m",
-    help="address of a repo manifest that will serve as the base")
-parser.add_argument("--branch", "-b", required=True,
+    help="repo manifest url suffix that will serve as the base")
+parser.add_argument("--branch", "-b",
     help="target branch to carbon date")
 parser.add_argument("--repopath",
-    help="path to create for the repo, if not specified will use curdir")
+    help="path to use as the base for the repo, if not specified will use curdir")
 
 args = parser.parse_args()
 
 def date_git(gitrepo_path, targdate):
     g = git.Git(gitrepo_path)
 
-    # don't technically need it, but the following line gets the date
-    # g.log("--before=\"{}\"".format(targdate), "-n", "1", "--format=%ci")
-    
     commitstr = g.log("--before=\"{}\"".format(targdate), "-n", "1", "--format=%H")
+    if commitstr == "":
+        commitdate = g.log("-n", "1", "--format=%ci")
+        print("WARNING: no commit for repo {} before {}, earliest {}".format(gitrepo_path, targdate, commitdate))
+        return
+    
+    # don't technically need it, but the following line gets the date
+    commitdate = g.log("--before=\"{}\"".format(targdate), "-n", "1", "--format=%ci")
+    
+    print("repo {} checking out commit {} dated {}".format(gitrepo_path, commitstr, commitdate))
     g.checkout("{}".format(commitstr))
 
-def main():
-    repopath = os.path.curdir
+def update_git(urlbase, assignpath, reponame, repobranch, targdate):
+    cwd = os.path.abspath(os.path.curdir)
+    if os.path.exists(assignpath) == False:
+        os.makedirs(assignpath)
+        repo = git.Repo.clone_from("{}/{}".format(urlbase, reponame),
+            assignpath, branch=repobranch)
+    # os.chdir(assignpath)
+    # date_git(os.path.curdir, targdate)
+    date_git(assignpath, targdate)
+    os.chdir(cwd)
 
-    if (args.repman != None) and (args.gitrepo != None):
-        print("only a git repo or a repo manifest required")
-
-    if args.repopath:
-        os.makedirs(args.repopath)
-        repopath = args.repopath
-        os.chdir(repopath)
-    
-    if args.repman != None:
-        repo = git.Repo.clone_from("{}/{}".format(args.url, args.repman), repopath, branch=args.branch)
-        date_git(args.repman, args.date)
+def sync_date(urlbase, repomanifest, repopath, repobranch, targdate):
+    proc = Popen(["repo", "manifest"], stdout=PIPE)
+    default_str = proc.stdout.read()
+    try:
+        default_str = default_str.decode("utf-8")
+        default_xml = ET.fromstring(default_str)
+    except:
+        repo = git.Repo.clone_from("{}/{}".format(urlbase, repomanifest), repopath, branch=repobranch)
+        date_git(repomanifest, targdate)
         f = open("default.xml", "r")
         default_str = f.read()
         f.close()
         default_xml = ET.fromstring(default_str)
-        for i in default_xml.getchildren():
-            assignpath = i.get("path")
-            # if it has a path, then it will exists in the repository
-            if assignpath != None:
-                os.makedirs(assignpath)
-                repo = git.Repo.clone_from("{}/{}".format(args.url, i.get("name")),
-                    assignpath, branch=i.get("revision"))
-                os.chdir(assignpath)
-                date_git(os.path.curdir, args.date)
-                os.chdir(repopath)
-    elif args.gitrepo != None:
-        repo = git.Repo.clone_from("{}/{}".format(args.url, i.get("name")),
-            assignpath, branch=i.get("revision"))
-        os.chdir(assignpath)
-        date_git(os.path.curdir, args.date)
-        
+
+    for i in default_xml.getchildren():
+        assignpath = i.get("path")
+        # if it has a path, then it will exists in the repository
+        if assignpath != None:
+            update_git(urlbase, assignpath, i.get("name"), i.get("revision"), targdate)
+
+def main():
+    repopath = os.path.abspath(os.path.curdir)
+
+    # can't have a repo manifest and a git repo to update
+    if (args.repman != None) and (args.gitrepo != None):
+        print("only a git repo or a repo manifest required")
+
+    # if the repo output path doesn't exists, we will be pulling it.
+    if args.repopath != None:
+        if os.path.exists(args.repopath) == False:
+            os.makedirs(args.repopath)
+            repopath = args.repopath
+            os.chdir(repopath)
+    
+    if args.gitrepo != None:
+        update_git(args.url, args.gitrepo, args.gitrepo, args.branch)
+    else:
+        sync_date(args.url, args.repman, args.repopath, args.branch, args.date)
+
 if __name__ == "__main__":
     main()
